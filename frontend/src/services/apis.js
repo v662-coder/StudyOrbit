ab797c63dd946122a986d003558739cf4df5a646
v662-coder <83722974+v662-coder@users.noreply.github.com>
2025-12-18 23:56:42 +0530

    api url

--- frontend/src/services/apis.js
+++ frontend/src/services/apis.js
@@ -1,5 +1,5 @@
-// const BASE_URL = "https://studyorbit.onrender.com";
-const BASE_URL = "http://localhost:5000";
+const BASE_URL = "https://studyorbit.onrender.com";
+// const BASE_URL = "http://localhost:5000";
 
 /* ================= AUTH ================= */
 export const endpoints = {
